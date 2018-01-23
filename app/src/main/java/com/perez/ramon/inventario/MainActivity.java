05e27252df6a06f4bf2894c24efb96b36d57566f
RamonPR92 <ramonpr92@hotmail.com>
2018-01-23 17:50:03 -0600

    La vista principal ahora muestra, los registros en una lista, dentro de un scrollview

--- app/src/main/java/com/perez/ramon/inventario/MainActivity.java
+++ app/src/main/java/com/perez/ramon/inventario/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CursorAdapterProductos cursorAdapterProductos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Productos.inicializaBD(this);
+        cursorAdapterProductos = new CursorAdapterProductos(
+                this, Productos.obtenerTodosLosProductos());
     }
 }

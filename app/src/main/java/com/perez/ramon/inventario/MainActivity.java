f0d278566c2039bff9ca14496e0cc44865f1527e
RamonPR92 <ramonpr92@hotmail.com>
2018-01-23 18:34:44 -0600

    Se enlazan los datos del adaptador a un listView

--- app/src/main/java/com/perez/ramon/inventario/MainActivity.java
+++ app/src/main/java/com/perez/ramon/inventario/MainActivity.java
@@ -2,8 +2,11 @@ package com.perez.ramon.inventario;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
     private CursorAdapterProductos cursorAdapterProductos;
 
@@ -11,8 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Inicia la base de datos
         Productos.inicializaBD(this);
+        //El cursor se carga con la consulta
         cursorAdapterProductos = new CursorAdapterProductos(
                 this, Productos.obtenerTodosLosProductos());
+        //enlaza la vista con la variable lista
+        ListView listView =(ListView)findViewById(R.id.listView);
+        //enlazamos el adaptador con la lista
+        listView.setAdapter(cursorAdapterProductos);
+        //enlazamos la lista y un escuchador
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }

f96f34741b3ff7657d33958702af6d44418a4481
RamonPR92 <ramonpr92@hotmail.com>
2018-01-23 22:13:33 -0600

    Se creo un mecanismo para actualizar la lista dinamicamente al persivir un cambio en los datos

--- app/src/main/java/com/perez/ramon/inventario/MainActivity.java
+++ app/src/main/java/com/perez/ramon/inventario/MainActivity.java
@@ -12,8 +12,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private CursorAdapterProductos cursorAdapterProductos;
     private FloatingActionButton btnAgregar;
+    private ListView listView;
 
     private final int CREAR_PRODUCTO = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +29,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         cursorAdapterProductos = new CursorAdapterProductos(
                 this, Productos.obtenerTodosLosProductos());
         //enlaza la vista con la variable lista
-        ListView listView =(ListView)findViewById(R.id.listView);
+        listView =(ListView)findViewById(R.id.listView);
         //enlazamos el adaptador con la lista
         listView.setAdapter(cursorAdapterProductos);
         //enlazamos la lista y un escuchador
         listView.setOnItemClickListener(this);
+
 
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,11 +49,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == CREAR_PRODUCTO && resultCode == RESULT_OK){
             //Se creo el producto y debemos recargar la lista
+            actualizarLista();
         }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+    }
+
+    private void actualizarLista(){
+        //Se reasigna el cursor con la nueva informacion a el adaptador
+        cursorAdapterProductos.changeCursor(Productos.obtenerTodosLosProductos());
+        //Se actualiza la lista en un hilo separado
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                cursorAdapterProductos.notifyDataSetChanged();
+            }
+        });
     }
 }

218ab6905c8ab8a99fc78be6f06fc51a4ce5275f
RamonPR92 <ramonpr92@hotmail.com>
2018-01-23 19:27:39 -0600

    Se agrega el boton para lanzar el activity de registro de un nuevo producto

--- app/src/main/java/com/perez/ramon/inventario/MainActivity.java
+++ app/src/main/java/com/perez/ramon/inventario/MainActivity.java
@@ -1,5 +1,7 @@
 package com.perez.ramon.inventario;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +11,16 @@ import android.widget.ListView;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
     private CursorAdapterProductos cursorAdapterProductos;
+    private FloatingActionButton btnAgregar;
+
+    private final int CREAR_PRODUCTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAgregar = (FloatingActionButton)findViewById(R.id.btnAgregar);
+
         //Inicia la base de datos
         Productos.inicializaBD(this);
         //El cursor se carga con la consulta
@@ -25,6 +32,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         listView.setAdapter(cursorAdapterProductos);
         //enlazamos la lista y un escuchador
         listView.setOnItemClickListener(this);
+
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NuevoProducto.class);
+                startActivityForResult(intent,CREAR_PRODUCTO);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == CREAR_PRODUCTO && resultCode == RESULT_OK){
+            //Se creo el producto y debemos recargar la lista
+        }
     }
 
     @Override
